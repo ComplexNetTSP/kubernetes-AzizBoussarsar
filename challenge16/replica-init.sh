009dd6046831c01f1856336977ad380bbb0cd738
Aziz Boussarsar <118576414+AzizBoussarsar@users.noreply.github.com>
2025-01-31 11:52:20 +0100

    init script for mongodb replicaset added

---
+++ challenge16/replica-init.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+kubectl exec -it mongodb-0 -n aboussarsar -- mongosh <<EOF
+rs.initiate({
+  _id: "rs0",
+  members: [
+    { _id: 0, host: "mongodb-0.mongodb-service:27017" },
+    { _id: 1, host: "mongodb-1.mongodb-service:27017" },
+    { _id: 2, host: "mongodb-2.mongodb-service:27017" }
+  ]
+})
+rs.status()
+EOF
